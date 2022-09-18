3163e6ea31eeaad1d0d6459134ded1762c1a2e8c
Girish Donthoju <girish@innopivott.biz>
2022-09-18 23:12:25 +0530

    removed commented code

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -96,39 +96,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# @blueprint.route('/data_producers_test', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # If the user does not select a file, the browser submits an
-#         # empty file without a filename.
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(UPLOAD_FOLDER, filename))
-#             excelData = pandas.read_excel("/home/seera/Documents/per/esquare/apps/home/temp_uploads/"+filename,engine='openpyxl',dtype=object,header=None)
-#             # print("excelData",excelData.values.tolist())
-
-#             for itemww in excelData.values.tolist():
-#                 print(itemww)
-
-#             return redirect(url_for('download_file', name=filename))
-
-#     return '''
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form method=post enctype=multipart/form-data>
-#       <input type=file name=file>
-#       <input type=submit value=Upload>
-#     </form>
-#     '''
 
 @blueprint.route('/data_sources', methods=['GET', 'POST'])
 @login_required
